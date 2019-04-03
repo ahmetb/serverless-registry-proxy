ad1809b4284cdf0c76b8b1dc74d8dc22890f0b00
Ahmet Alp Balkan <ahmetb@google.com>
2019-04-03 11:55:54 -0700

    Refactor headers for simpler code
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httputil"
@@ -46,59 +44,53 @@ func main() {
 		projectID: gcrProjectID,
 	}
 
-	proxy := &httputil.ReverseProxy{
-		Transport: roundTripper(rt),
-		Director:  rewriteRegistryV2(gcr),
-	}
 	addr := ":" + port
-	log.Printf("starting to listen on %s", addr)
-	http.Handle("/v2/", proxy)
 	if browserRedirects {
-		http.HandleFunc("/", browserRedirectHandler(gcr))
+		http.Handle("/", browserRedirectHandler(gcr))
 	}
+	http.Handle("/v2/", registryAPIMux(gcr))
+	log.Printf("starting to listen on %s", addr)
 	if err := http.ListenAndServe(addr, nil); err != nil && err != http.ErrServerClosed {
 		log.Fatalf("listen error: %+v", err)
 	}
 	log.Printf("server shutdown successfully")
 }
 
-type roundTripper func(*http.Request) (*http.Response, error)
-
-func (f roundTripper) RoundTrip(req *http.Request) (*http.Response, error) { return f(req) }
-
-func rt(req *http.Request) (*http.Response, error) {
-	log.Printf("request received. url=%s", req.URL)
-
-	// TODO(ahmetb) remove after b/129780113 is fixed.
-	req.Header.Del("accept")
-	req.Header.Set("accept", "*/*")
-
-	// fabricate 200 OK response for /v2/ endpoint
-	if req.URL.Path == "/v2/" {
-		resp := &http.Response{
-			Request: req,
-			Header: map[string][]string{
-				"Docker-Distribution-API-Version": []string{"registry/2.0"},
-			},
-			Body:       ioutil.NopCloser(bytes.NewReader(nil)),
-			StatusCode: http.StatusOK,
-			Status:     http.StatusText(http.StatusOK),
-			Proto:      req.Proto,
-			ProtoMinor: req.ProtoMinor,
-			ProtoMajor: req.ProtoMajor,
-		}
-		return resp, nil
+// browserRedirectHandler redirects a request like example.com/my-image to
+// gcr.io/my-image, which shows a public UI for browsing the registry.
+func browserRedirectHandler(c gcrConfig) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		url := fmt.Sprintf("https://%s/%s%s", c.host, c.projectID, r.RequestURI)
+		http.Redirect(w, r, url, http.StatusTemporaryRedirect)
 	}
-
-	resp, err := http.DefaultTransport.RoundTrip(req)
-	if err == nil {
-		log.Printf("request completed (status=%d) url=%s", resp.StatusCode, req.URL)
-	} else {
-		log.Printf("request failed with error: %+v", err)
-	}
-	return resp, err
 }
 
+// registryAPIMux returns a handler for Docker Registry v2 API requests
+// (/v2/). Request to path=/v2/ is handled-locally, other /v2/* requests are
+// proxied back to GCR endpoint.
+func registryAPIMux(c gcrConfig) http.HandlerFunc {
+	reverseProxy := &httputil.ReverseProxy{
+		Transport: roundtripperFunc(gcrRoundtripper),
+		Director:  rewriteRegistryV2(c),
+	}
+
+	return func(w http.ResponseWriter, req *http.Request) {
+		if req.URL.Path == "/v2/" {
+			handleRegistryAPIVersion(w, req)
+			return
+		}
+		reverseProxy.ServeHTTP(w, req)
+	}
+}
+
+// handleRegistryAPIVersion signals docker-registry v2 API on /v2/ endpoint.
+func handleRegistryAPIVersion(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Docker-Distribution-API-Version", "registry/2.0")
+	fmt.Fprint(w, "ok")
+}
+
+// rewriteRegistryV2 rewrites request.URL like /v2/* that come into the server
+// into https://[GCR_HOST]/v2/[PROJECT_ID]/*. It leaves /v2/ as is.
 func rewriteRegistryV2(c gcrConfig) func(*http.Request) {
 	return func(req *http.Request) {
 		u := req.URL.String()
@@ -112,11 +104,21 @@ func rewriteRegistryV2(c gcrConfig) func(*http.Request) {
 	}
 }
 
-// browserRedirectHandler redirects a request like example.com/my-image to
-// gcr.io/my-image, which shows a public UI for browsing the registry.
-func browserRedirectHandler(c gcrConfig) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		url := fmt.Sprintf("https://%s/%s%s", c.host, c.projectID, r.RequestURI)
-		http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+type roundtripperFunc func(*http.Request) (*http.Response, error)
+
+func (f roundtripperFunc) RoundTrip(req *http.Request) (*http.Response, error) { return f(req) }
+
+func gcrRoundtripper(req *http.Request) (*http.Response, error) {
+	log.Printf("request received. url=%s", req.URL)
+
+	// TODO(ahmetb) remove after internal bug 129780113 is fixed.
+	req.Header.Set("accept", "*/*")
+
+	resp, err := http.DefaultTransport.RoundTrip(req)
+	if err == nil {
+		log.Printf("request completed (status=%d) url=%s", resp.StatusCode, req.URL)
+	} else {
+		log.Printf("request failed with error: %+v", err)
 	}
+	return resp, err
 }
